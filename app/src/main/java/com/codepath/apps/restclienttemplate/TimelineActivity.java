23f1d52629ed8c5bc3eab255d59112a569cd0dd3
Endi Caushi <mrendi29@gmail.com>
2019-02-02 02:29:07 -0500

    Trying to add endless pagination Part 2

--- app/src/main/java/com/codepath/apps/restclienttemplate/TimelineActivity.java
+++ app/src/main/java/com/codepath/apps/restclienttemplate/TimelineActivity.java
@@ -26,6 +26,7 @@ public class TimelineActivity extends AppCompatActivity {
     private List<Tweet> tweets;
     private TwitterClient client;
     private SwipeRefreshLayout swipeContainer;
+    private EndlessRecyclerViewScrollListener scrollListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,7 +42,6 @@ public class TimelineActivity extends AppCompatActivity {
                 android.R.color.holo_red_light);
 
 
-
         //Find the recycler view.
         rvTweets = findViewById(R.id.rvTweets);
 
@@ -50,7 +50,8 @@ public class TimelineActivity extends AppCompatActivity {
         adapter = new TweetAdapter(this, tweets);
 
         //Set up recycler view.
-        rvTweets.setLayoutManager(new LinearLayoutManager(this));
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this);
+        rvTweets.setLayoutManager(linearLayoutManager);
         rvTweets.setAdapter(adapter);
 
         client = TwitterApp.getRestClient(this);
@@ -63,8 +64,57 @@ public class TimelineActivity extends AppCompatActivity {
                 populateHomeTimeline();
             }
         });
-        
 
+        scrollListener = new EndlessRecyclerViewScrollListener(linearLayoutManager) {
+            @Override
+            public void onLoadMore(int page, int totalItemsCount, RecyclerView view) {
+
+                loadNextDataFromApi(page);
+
+            }
+        };
+        // Adds the scroll listener to RecyclerView
+        rvTweets.addOnScrollListener(scrollListener);
+
+    }
+
+    private void loadNextDataFromApi(int offset) {
+        client.getHomeTimeline(new JsonHttpResponseHandler() {
+            @Override
+            public void onSuccess(int statusCode, Header[] headers, JSONArray response) {
+                Log.d("sucess1", response.toString());
+
+                List<Tweet> tweetsToAppend = new ArrayList<>();
+                //Iterate through the list of tweets that comes from response
+                for (int i = 0; i < response.length(); ++i) {
+
+                    try {
+                        //Convert each jsonObject into a tweet object
+                        JSONObject jsonObject = response.getJSONObject(i);
+                        Tweet tweet = Tweet.fromJson(jsonObject);
+                        //Add the tweet into our data source
+                        tweetsToAppend.add(tweet);
+
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+                }
+                adapter.clear();
+                adapter.addTweets(tweetsToAppend);
+                adapter.notifyItemRangeInserted(adapter.getItemCount(), tweets.size() - 1);
+
+            }
+
+            @Override
+            public void onFailure(int statusCode, Header[] headers, String responseString, Throwable throwable) {
+                Log.e("failure1", throwable.toString());
+            }
+
+            @Override
+            public void onFailure(int statusCode, Header[] headers, Throwable throwable, JSONArray errorResponse) {
+                Log.e("failure1", throwable.toString());
+            }
+        });
     }
 
     private void populateHomeTimeline() {
